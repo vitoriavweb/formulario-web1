b1a2cda3c616d0303506919a572a5b39700d2407
vitoriavweb <124357303+vitoriavweb@users.noreply.github.com>
2023-06-16 10:49:27 -0300

    Add files via upload

--- formularioweb/js/bb.js
+++ formularioweb/js/bb.js
@@ -1,24 +1,3 @@
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-  }
-  
-  function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  }
-  
   function validarFormulario(event) {
     event.preventDefault();
   
@@ -66,13 +45,72 @@ function setCookie(name, value, days) {
   
       setCookie('nome', nome, 7);
       setCookie('id', id, 7);
+
   
       alert('Cadastro realizado com sucesso!');
-      document.getElementById('formulario').reset();
+     // document.getElementById('formulario').reset();
     } catch (error) {
       alert(error.message);
     }
   }
   
+  
   document.getElementById('formulario').addEventListener('submit', validarFormulario);
+
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+  
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  //function cookie()
+  // Verificar se os cookies estão definidos
+  var username = getCookie("username");
+  var userid = getCookie("userid");
+  
+  if (username && userid) {
+    // Exibir nome e ID do usuário na tela
+    var dadosUsuarioElement = document.getElementById("dadosUsuario");
+    dadosUsuarioElement.innerHTML = "Nome do usuário: " + username + "<br>" + "ID do usuário: " + userid;
+  }
+  
+  // Capturar o evento de envio do formulário
+  var formularioElement = document.getElementById("formulario");
+  formularioElement.addEventListener("submit", function (event) {
+    event.preventDefault(); // Impedir o envio do formulário
+  
+    // Obter os valores dos campos
+    var nome = document.getElementById("nome").value;
+    var id = document.getElementById("id").value;
+  
+    // Salvar os dados nos cookies
+    setCookie("username", nome, 7);
+    setCookie("userid", id, 7);
+  
+    // Exibir nome e ID do usuário na tela
+    var dadosUsuarioElement = document.getElementById("dadosUsuario");            
+    dadosUsuarioElement.innerHTML = "Nome do usuário: " + nome + "<br>" + "ID do usuário: " + id;
+    document.innerHTML(nome);
+    
+  
+    // Limpar os campos do formulário
+  // formularioElement.reset();
+  });  
+
+  
   
